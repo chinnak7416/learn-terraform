eb0f58b69a92e5d8ec9a2f000cfc7095b73b5b0c
RAMBABU <chinnak7416@gmail.com>
2024-03-17 22:54:02 +0530

    demo on loop

--- 07-loop/loop.tf
+++ 07-loop/loop.tf
@@ -1,3 +1,17 @@
 resource "null_resource" "test" {
   count = 10
 }
+resource "null_resource" "test" {
+  for_each = var.colors-code
+}
+
+variable "colors-code" {
+  default = {
+    red = 31
+    green = 32
+    yellow = 33
+    blue = 34
+    magenta = 35
+    cyan = 36
+  }
+}
