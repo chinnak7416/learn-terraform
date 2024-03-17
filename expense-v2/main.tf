d7cc2a91b1049f80ca0c7e54f0ad3150cc5c124f
RAMBABU <chinnak7416@gmail.com>
2024-03-17 23:06:35 +0530

    code dry concept for ec2

---
+++ expense-v2/main.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "ec2" {
+  for_each = var.instance_types
+  ami           = var.ami
+  instance_type = each.value["instance_type"]
+  vpc_security_group_ids = var.security_groups
+  tags = {
+    Name = each.key
+  }
+}
+
+variable "ami" {
+  default = "ami-05f020f5935e52dc4"
+}
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "security_groups" {
+  default = ["sg-00d3e3d7744a86ad7"]
+}
+variable "instance_types" {
+  default = {
+    frontend = {
+      instance_type = "t3.micro"
+    }
+    backend = {
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      instance_type = "t3.micro"
+    }
+  }
+}
