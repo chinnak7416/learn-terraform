6f6f8ef0ddad64b40e79469158a690460ca93c2e
RAMBABU <chinnak7416@gmail.com>
2024-03-23 08:49:55 +0530

    demo on state file

---
+++ 08-state/main.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state78"
+    key    = "testing/state"
+    region = "us-east-1"
+  }
+}
+
+resource "null_resource" "dummy1" {}
+resource "null_resource" "dummy2" {}
