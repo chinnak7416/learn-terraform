3d50d14415dae3cbd113a5189c64bc00d19d4a02
RAMBABU <chinnak7416@gmail.com>
2024-06-11 22:06:26 +0530

    Revert "Adding  Vault to the project"
    This reverts commit 22f9b83e3ee4304818ffb42919551f6fa74759bd.

--- 08-state/main.tf
+++ 08-state/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tf-78-state"
+    bucket = "terraform-state78"
     key    = "testing/state"
     region = "us-east-1"
   }
--- 09-multi-state/prod-backend.tfvars
+++ 09-multi-state/prod-backend.tfvars
@@ -1,3 +1,3 @@
-bucket = "tf-78-state"
+bucket = "terraform-state78"
 key    = "testing/prod/state"
 region = "us-east-1"
